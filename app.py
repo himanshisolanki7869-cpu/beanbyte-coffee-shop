fbcbe21654fb37f38a65aaff1568ea5aa9dbb42d
Himanshi Solanki <himanshisolanki7869@gmail.com>
2026-06-25 09:54:54 +0530

    Prepare project for Render deployment

--- app.py
+++ app.py
@@ -657,7 +657,7 @@ def admin():
 
 init_db()
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="127.0.0.1", port=port, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+app = app
